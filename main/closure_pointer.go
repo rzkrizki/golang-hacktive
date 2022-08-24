8c07cee0326af1de5a5d12489b12470a4e4fa35d
Rizki Ramadhan <rizkiramadhan6494848@gmail.com>
2022-08-24 19:05:40 +0700

    push closer + pointer

---
+++ main/closure_pointer.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func closuser_pointer() {
+	fajar := "fajar"
+	agus := "agus"
+	maulana := "maulana"
+	zakaria := "zakaria"
+	wahyu := "wahyu"
+	zayyan := "zayyan"
+	rizki := "rizki"
+	ramadhan := "ramadhan"
+	billy := "billy"
+	hendar := "hendar"
+
+	lists := []*string{&fajar, &agus, &maulana, &zakaria, &wahyu, &zayyan, &rizki, &ramadhan, &billy, &hendar}
+
+	var cetak = func(lists []*string) []string {
+
+		var results []string
+
+		for _, n := range lists {
+			results = append(results, *n)
+		}
+
+		return results
+	}
+
+	fmt.Println(cetak(lists))
+}
