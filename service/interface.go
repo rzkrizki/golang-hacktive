abe2818a9e5d5eed2cb64467464e03addda5caad
Rizki Ramadhan <rizkiramadhan6494848@gmail.com>
2022-08-29 20:46:51 +0700

    wg and go routine

--- service/interface.go
+++ service/interface.go
@@ -1,6 +1,7 @@
 package service
 
 type User struct {
+	ID   int
 	Nama string
 }
 
@@ -11,6 +12,7 @@ type UserService struct {
 type UserIface interface {
 	Register(u *User) string
 	GetUser() []*User
+	GetUserbyID(number int) *User
 }
 
 func NewUserService(db []*User) UserIface {
@@ -27,3 +29,32 @@ func (u *UserService) Register(user *User) string {
 func (u *UserService) GetUser() []*User {
 	return u.db
 }
+
+func (u *UserService) GetUserbyID(number int) *User {
+	for _, v := range u.db {
+		if v.ID == number {
+			return v
+		}
+	}
+	return nil
+}
+
+// func main() {
+// 	var db []*service.User
+// 	userSrv := service.NewUserService(db)
+// 	user1 := userSrv.Register(&service.User{ID: 1, Nama: "rizki"})
+// 	fmt.Println(user1)
+// 	user2 := userSrv.Register(&service.User{ID: 2, Nama: "zaka"})
+// 	fmt.Println(user2)
+// 	user3 := userSrv.Register(&service.User{ID: 3, Nama: "fajar"})
+// 	fmt.Println(user3)
+// 	fmt.Println("-----------Hasil get user-------------")
+// 	resGet := userSrv.GetUser()
+// 	for _, v := range resGet {
+// 		fmt.Println(v.ID, v.Nama)
+// 	}
+
+// 	fmt.Println("-----------Hasil get user by id-------------")
+// 	getusr1 := userSrv.GetUserbyID(&service.User{ID: 1})
+// 	fmt.Println(getusr1)
+// }
