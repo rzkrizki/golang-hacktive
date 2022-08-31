c9024ccfd9b90e87960fe27c93ea0b196e797c25
Rizki Ramadhan <rizkiramadhan6494848@gmail.com>
2022-08-31 20:02:11 +0700

    web service

---
+++ web-service/web-service.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"golang-hacktive/service"
+	"net/http"
+)
+
+var port = ":6969"
+
+func WebService() {
+	var db []*service.User
+	userSrv := service.NewUserService(db)
+	http.HandleFunc("/register", userSrv.RegisterUser)
+	http.HandleFunc("/get-user/", userSrv.GetUserRegister)
+	http.HandleFunc("/getuserbyid", userSrv.GetUserRegisterbyID)
+	http.ListenAndServe(port, nil)
+}
